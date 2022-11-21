dc7ab770d8b4d2b5dad95e9fb88376f33d89ace1
Alina Ali01~ <alina.zenam@hotmail.com>
2022-11-21 14:01:42 +0000

    entire code

--- script.js
+++ script.js
@@ -88,19 +88,54 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var pwLength = 0; 
+var userChoices = [ ];
+var results = "";
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+while (parseInt(pwLength)<10||parseInt(pwLength)>64||parseInt(pwLength)==''||isNaN(parseInt(pwLength))) { 
+pwLength=prompt('please choose a password length between 10 and 64 characters')
+}
+var uppercaseConfirmation = confirm('would you like uppercase letters in your password?')
+if (uppercaseConfirmation === true) { 
+  userChoices = userChoices.concat(upperCasedCharacters)
+  
+}
+var lowercaseConfirmation = confirm('would you like lowercase letters in your password?')
+if (lowercaseConfirmation === true) { 
+  userChoices = userChoices.concat(lowerCasedCharacters)
+  
+}
+var numericConfirmation = confirm('would you like numbers in your password?')
+if (numericConfirmation === true) { 
+  userChoices = userChoices.concat(numericCharacters)
+  
+}
+var specialCharConfirmation = confirm('would you like special characters in your password?')
+if (specialCharConfirmation === true) { 
+  userChoices = userChoices.concat(specialCharacters)
+  
+}
+if (uppercaseConfirmation===false&&lowercaseConfirmation===false&&numericConfirmation===false&&specialCharConfirmation===false) {
+  alert('you have to choose at least one type of character to generate your password')
+  window.location.reload()
+}
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+// function getRandom(arr) {
+//   arr[Math.floor(Math.random()*arr.length)]
+// }
 
 // Function to generate password with user input
 function generatePassword() {
+getPasswordOptions() 
 
+for (let i=0, n=userChoices.length; i<parseInt(pwLength); i++) {
+results+=userChoices[Math.floor(Math.random()*n)]
+}
+return results
 }
 
 // Get references to the #generate element
